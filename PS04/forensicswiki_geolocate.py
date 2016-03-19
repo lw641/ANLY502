b2b3f4ec396dc905167878bc4dbada1c25e56f1c
lw641 <lw641@georgetown.edu>
2016-03-18 20:49:00 -0400

    Update forensicswiki_geolocate.py

--- PS04/forensicswiki_geolocate.py
+++ PS04/forensicswiki_geolocate.py
@@ -5,24 +5,16 @@ import sys
 from operator import add
 from pyspark import SparkContext
 
-
-def isokay(ch):
-    return ch in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '
-
 if __name__ == "__main__":
  
+    sc = SparkContext( appName="Geolocate" )
     infile =  's3://gu-anly502/ps04/Shakespeare.txt'
-
-    sqlContext = SQLContext(sc)
- 
     myfile = sc.textFile("s3://gu-anly502/maxmind/GeoLite2-Country-Blocks-IPv4.csv")
-    
-    map1 = myfile.map(lambda line: line.split(','))
-    map1 = myfile.map(lambda line: line.split(',')).map(lambda x: (x[1],x))
-    
     myfile2 = sc.textFile("s3://gu-anly502/maxmind/GeoLite2-Country-Locations-en.csv")
-    map2 = myfile2.map(lambda line: line.split(','))
-    map2 = myfile2.map(lambda line: line.split(',')).map(lambda x: (x[0],x))
+    
+    map1 = myfile.zipWithIndex().filter(lambda x:x[1]>0).map(lambda x:x[0]).map(lambda line: line.split(',')).map(lambda x: (x[1],x))
+
+    map2 = myfile2.zipWithIndex().filter(lambda x:x[1]>0).map(lambda x:x[0]).map(lambda line: line.split(',')).map(lambda x: (x[0],x))
   
    
     top40counts = counts.sortBy(lambda x: x[1], ascending=False).take(40)
